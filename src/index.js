d0a1a9e23001d30f4b6f85725383fdceb0bfaca7
andreyselin <andreiprostoandrei@gmail.com>
2020-08-31 19:41:50 +0200

    finalize simple set up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 // app.use(bodyParser.json());
 app.use(express.json());
 
 // POST method route
-app.post('/', function (req, res) {
-    res.send('POST request to the homepage')
+app.post('/', async function (req, res) {
+    console.log('request', req);
+    const result = await axios.get('https://getnotifiedbot.com/notify/vL4eRFqNO/?message=okayMAN');
+    res.send('req.body');
 });
 
-app.listen(25252);
+app.listen(2525);
